907cca56f47dbcef9d9dde394073fea57ca134d8
Dhruv Gautam <dhruvgautam815@gmail.com>
2026-07-10 16:43:05 +0530

    Time: 83 ms (91.67%), Space: 270.8 MB (83.33%) - LeetHub

---
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[] pos = new int[n];
+        for (int i = 0; i < n; i++) {
+            pos[arr[i][1]] = i;
+        }
+
+        int LOG = 18;
+        int[][] jump = new int[LOG][n];
+
+        int r = 0;
+        for (int l = 0; l < n; l++) {
+            while (r + 1 < n && arr[r + 1][0] - arr[l][0] <= maxDiff) {
+                r++;
+            }
+            jump[0][l] = r;
+        }
+
+        for (int k = 1; k < LOG; k++) {
+            for (int i = 0; i < n; i++) {
+                jump[k][i] = jump[k - 1][jump[k - 1][i]];
+            }
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int qi = 0; qi < queries.length; qi++) {
+            int u = pos[queries[qi][0]];
+            int v = pos[queries[qi][1]];
+
+            if (u > v) {
+                int t = u;
+                u = v;
+                v = t;
+            }
+
+            if (u == v) {
+                ans[qi] = 0;
+                continue;
+            }
+
+            if (jump[0][u] == u) {
+                ans[qi] = -1;
+                continue;
+            }
+
+            int cur = u;
+            int steps = 0;
+
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (jump[k][cur] < v) {
+                    cur = jump[k][cur];
+                    steps += 1 << k;
+                }
+            }
+
+            if (jump[0][cur] >= v)
+                ans[qi] = steps + 1;
+            else
+                ans[qi] = -1;
+        }
+
+        return ans;
+    }
+}
