e4ed740253445455169562509b1586089c7dfd04
Dhruv Gautam <dhruvgautam815@gmail.com>
2026-08-01 17:26:12 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.79%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        // Base case: one element
+        for (int i = 0; i < n; i++) {
+            dp[i] = nums[i];
+        }
+
+        // Length of subarray
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+
+                dp[i] = Math.max(
+                    nums[i] - dp[i + 1],
+                    nums[j] - dp[i]
+                );
+            }
+        }
+
+        return dp[0] >= 0;
+    }
+}
